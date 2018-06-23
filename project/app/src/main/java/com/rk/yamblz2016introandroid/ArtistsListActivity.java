adf3ce960948dbff4af4bc6ecf66e3c4e1ca35c0
maksimn <maksim1989sovunion@yandex.ru>
2018-06-23 11:33:06 +0300

    Volley HTTP request to get JSON data about artists.

--- project/app/src/main/java/com/rk/yamblz2016introandroid/ArtistsListActivity.java
+++ project/app/src/main/java/com/rk/yamblz2016introandroid/ArtistsListActivity.java
@@ -3,11 +3,32 @@ package com.rk.yamblz2016introandroid;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.android.volley.*;
+import com.android.volley.toolbox.*;
+
 public class ArtistsListActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_artists_list);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://download.cdn.yandex.net/mobilization-2016/artist678s.json";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+        });
+
+        queue.add(stringRequest);
     }
 }
