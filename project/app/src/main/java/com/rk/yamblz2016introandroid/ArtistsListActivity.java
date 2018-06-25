9a38720b96af4f0c0d21e008749212fe50718027
maksimn <maksim1989sovunion@yandex.ru>
2018-06-25 22:41:18 +0300

    Toolbar (app bar) with right background and font colors

--- project/app/src/main/java/com/rk/yamblz2016introandroid/ArtistsListActivity.java
+++ project/app/src/main/java/com/rk/yamblz2016introandroid/ArtistsListActivity.java
@@ -1,17 +1,20 @@
 package com.rk.yamblz2016introandroid;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 
 import com.android.volley.*;
 import com.android.volley.toolbox.*;
 
-public class ArtistsListActivity extends Activity {
+public class ArtistsListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_artists_list);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         RequestQueue queue = Volley.newRequestQueue(this);
         String url = getString(R.string.artists_url);;
